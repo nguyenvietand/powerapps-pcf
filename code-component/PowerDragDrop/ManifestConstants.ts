9a052eb8a0ec951dd3676232b8e050f9d25747ff
An Nguyen <vietan7789@gmail.com>
2026-03-25 23:05:48 +0700

    fix security vulnerabilities

---
+++ code-component/PowerDragDrop/ManifestConstants.ts
@@ -0,0 +1,115 @@
+export enum ItemProperties {
+    IdColumn = 'IdColumn',
+    ZoneColumn = 'ZoneColumn',
+    CustomPositionColumn = 'CustomPositionColumn',
+}
+
+export enum ManifestConstants {
+    RotateOnDrag = 'RotateOnDrag',
+    DropZoneID = 'DropZoneID',
+    dataset = 'dataset',
+    PreserveSort = 'PreserveSort',
+    OtherDropZoneIDs = 'OtherDropZoneIDs',
+    IsMasterZone = 'IsMasterZone',
+    ItemBackgroundColor = 'ItemBackgroundColor',
+    ItemFontSize = 'ItemFontSize',
+    ItemFontColor = 'ItemFontColor',
+    MaximumItems = 'MaximumItems',
+    InputEvent = 'InputEvent',
+    Scroll = 'Scroll',
+    ItemTemplate = 'ItemTemplate',
+    PaddingRight = 'PaddingRight',
+    PaddingLeft = 'PaddingLeft',
+    PaddingTop = 'PaddingTop',
+    PaddingBottom = 'PaddingBottom',
+    BackgroundColor = 'BackgroundColor',
+    BorderColor = 'BorderColor',
+    BorderWidth = 'BorderWidth',
+    BorderRadius = 'BorderRadius',
+    ItemBorderColor = 'ItemBorderColor',
+    ItemBorderWidth = 'ItemBorderWidth',
+    ItemBorderRadius = 'ItemBorderRadius',
+    ItemFont = 'ItemFont',
+    DelaySelect = 'DelaySelect',
+    ItemGap = 'ItemGap',
+    Direction = 'Direction',
+    Wrap = 'Wrap',
+    AccessibleLabel = 'AccessibleLabel',
+    AllowFocus = 'AllowFocus',
+    SortPositionType = 'SortPositionType',
+}
+
+export enum InputEvents {
+    Reset = 'Reset',
+    ClearChanges = 'ClearChanges',
+    SetFocus = 'SetFocus',
+    FocusItem = 'FocusItem',
+    SyncPositions = 'SyncPositions',
+}
+
+export enum OutputEvents {
+    OnDrop = 'OnDrop',
+    OnAction = 'OnAction',
+    OnDropAfterSyncPositions = 'OnDropAfterSyncPositions',
+}
+
+export const RENDER_TRIGGER_PROPERTIES: string[] = [
+    ManifestConstants.DropZoneID,
+    ManifestConstants.OtherDropZoneIDs,
+    ManifestConstants.IsMasterZone,
+    ManifestConstants.BackgroundColor,
+    ManifestConstants.BorderRadius,
+    ManifestConstants.BorderColor,
+    ManifestConstants.BorderWidth,
+    ManifestConstants.ItemBackgroundColor,
+    ManifestConstants.ItemFont,
+    ManifestConstants.ItemFontSize,
+    ManifestConstants.ItemFontColor,
+    ManifestConstants.ItemBorderColor,
+    ManifestConstants.ItemBorderWidth,
+    ManifestConstants.ItemBorderRadius,
+    ManifestConstants.ItemGap,
+    ManifestConstants.ItemTemplate,
+    ManifestConstants.PaddingRight,
+    ManifestConstants.PaddingLeft,
+    ManifestConstants.PaddingTop,
+    ManifestConstants.PaddingBottom,
+    ManifestConstants.Scroll,
+    ManifestConstants.Direction,
+    ManifestConstants.Wrap,
+    ManifestConstants.AccessibleLabel,
+    ManifestConstants.AllowFocus,
+    ManifestConstants.DelaySelect,
+    ManifestConstants.SortPositionType,
+];
+
+export const ZONE_REGISTRATION_PROPERTIES: string[] = [
+    ManifestConstants.IsMasterZone,
+    ManifestConstants.DropZoneID,
+    ManifestConstants.OtherDropZoneIDs,
+    ManifestConstants.DelaySelect,
+];
+
+export const ZONE_OPTIONS_PROPERTIES: string[] = [
+    ManifestConstants.RotateOnDrag,
+    ManifestConstants.MaximumItems,
+    ManifestConstants.Scroll,
+    ManifestConstants.DelaySelect,
+    ManifestConstants.PreserveSort,
+];
+
+export enum DirectionEnum {
+    Auto = '0',
+    Vertical = '1',
+    Horizontal = '2',
+}
+
+export enum SortDirection {
+    Ascending = '0',
+    Descending = '1',
+}
+
+export enum SortPositionType {
+    Index = '0',
+    Custom = '1',
+}
---
+++ code-component/PowerDragDrop/Styles.ts
@@ -0,0 +1,28 @@
+export const RECORD_ID_ATTRIBUTE = 'data-id';
+export const ORIGINAL_POSITION_ATTRIBUTE = 'data-original-position';
+export const ORIGINAL_SORT_ORDER_ATTRIBUTE = 'data-original-sort-position';
+export const ORIGINAL_ZONE_ATTRIBUTE = 'data-original-zone';
+export const DRAGGED_FROM_ZONE_ATTRIBUTE = 'data-dragged-from-zone';
+export const RENDER_VERSION_ATTRIBUTE = 'data-render-version';
+export const DRAG_INVALID = 'powerdnd-drag-invalid';
+export const ROTATION_CLASSES = [
+    'powerdnd-drag-rotate-clockwise-small',
+    'powerdnd-drag-rotate-clockwise-large',
+    'powerdnd-drag-rotate-anticlockwise-small',
+    'powerdnd-drag-rotate-anticlockwise-large',
+];
+
+export enum CSS_STYLE_CLASSES {
+    MainContainer = 'powerdnd-main-container',
+    List = 'powerdnd-list',
+    Ghost = 'powerdnd-ghost',
+    Drag = 'powerdnd-drag',
+    Item = 'powerdnd-item',
+    ItemSimple = 'powerdnd-item-simple',
+    ItemValue = 'powerdnd-item-value',
+    ActionClassPrefix = 'action-',
+    Chosen = 'powerdnd-chosen',
+    WarningContainer = 'powerdnd-warning-container',
+    Warning = 'powerdnd-warning',
+    WarningIcon = 'powerdnd-warning-icon',
+}
